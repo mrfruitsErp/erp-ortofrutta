e481a384d3ee0a52ef9dbb08f4110f8a7191a12f
ERP Ortofrutta <erp@locale.com>
2026-03-30 11:09:33 +0200

    feat: script migrate via browser

---
+++ public/migrate.php
@@ -0,0 +1,9 @@
+cd C:\xampp\htdocs\gestionale
+git add public/migrate.php
+git commit -m "feat: script migrate via browser"
+git push origin main
+```
+
+Dopo il deploy vai su:
+```
+https://erp.mrfruits.it/migrate.php?token=mrfruits_migrate_2026
